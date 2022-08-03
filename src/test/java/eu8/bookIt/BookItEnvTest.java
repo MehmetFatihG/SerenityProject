c95fbbe71cd9f2cc134454561199855e9cb9141f
fatih <goksanfatih@gmail.com>
2022-08-03 23:48:53 +0300

    commit

---
+++ src/test/java/eu8/bookIt/BookItEnvTest.java
@@ -0,0 +1,18 @@
+package eu8.bookIt;
+
+import net.serenitybdd.junit5.SerenityTest;
+import org.junit.jupiter.api.Test;
+import utilities.ConfigReader;
+
+@SerenityTest
+public class BookItEnvTest {
+
+    @Test
+    public void test1(){
+
+        System.out.println(ConfigReader.getProperty("base.url"));
+        System.out.println(ConfigReader.getProperty("teacher_email"));
+        System.out.println(ConfigReader.getProperty("teacher_password"));
+    }
+
+}
